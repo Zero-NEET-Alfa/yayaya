d82003381d47ee5f57c167a8732fb7e2a094266c
Zero-NEET-Alfa <danidaboy54@gmail.com>
2020-12-13 15:27:00 +0700

    update builder
    Signed-off-by: Zero-NEET-Alfa <danidaboy54@gmail.com>

---
+++ do-bn-u.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+branch="20201208/BN-U"
+BuilderKernel="00000"
+
+if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] ;then
+    exit;
+fi
+. main.sh 'initial' 'full'
+
+FolderUp="BrokenNucleus"
+spectrumFile="f.rc"
+TypeBuild="Stable"
+TypeBuildTag="AOSP"
+getInfo ">> Building kernel . . . . <<"
+
+CompileKernel
+CompileKernel "65"
+# CompileKernel "68"
+# CompileKernel "71"
+# CompileKernel "72"
+
+FixPieWifi
+
+CompileKernel
+CompileKernel "65"
+# CompileKernel "68"
+# CompileKernel "71"
+# CompileKernel "72"
+
+tg_send_info "All $GetKernelName $BuilderKernel already uploaded to Gdrive :D"
