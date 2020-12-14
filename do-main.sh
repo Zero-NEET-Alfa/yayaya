5ed83ec908ad2f9ef5df33c7bd943ffd6c8f1280
Zero-NEET-Alfa <danidaboy54@gmail.com>
2020-12-14 22:20:36 +0700

    do-main: more faster build kranel
    Signed-off-by: Zero-NEET-Alfa <danidaboy54@gmail.com>

--- do-main.sh
+++ do-main.sh
@@ -5,24 +5,24 @@ BuilderKernel="00000"
 if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] ;then
     exit;
 fi
-. main.sh 'initial' 'full'
+. main.sh 'initial'
 
 FolderUp="BrokenNucleus"
-spectrumFile="f.rc"
-TypeBuild="Stable"
+spectrumFile=""
+TypeBuild="MAIN-BRANCH"
 TypeBuildTag="AOSP"
 getInfo ">> Building kernel . . . . <<"
 
 CompileKernel
-CompileKernel "65"
+# CompileKernel "65"
 # CompileKernel "68"
 # CompileKernel "71"
 # CompileKernel "72"
 
-FixPieWifi
+# FixPieWifi
 
-CompileKernel
-CompileKernel "65"
+# CompileKernel
+# CompileKernel "65"
 # CompileKernel "68"
 # CompileKernel "71"
 # CompileKernel "72"
