8ad488771e747ac9c43101ab6f94d5c4aae2f138
Zero-NEET-Alfa <danidaboy54@gmail.com>
2021-01-04 07:51:07 +0700

    update
    Signed-off-by: Zero-NEET-Alfa <danidaboy54@gmail.com>

--- do-main2.sh
+++ do-main2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-# branch="20201215/main"
-branch="20201215/Neutrino-X"
+branch="20201215/main"
+# branch="20201215/Neutrino-X"
 BuilderKernel="00000"
 
 if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] ;then
@@ -11,13 +11,13 @@ fi
 Author="ZyCromerZ"
 FolderUp="x01bd"
 ExFolder="Neutrino"
-# spectrumFile="None"
-spectrumFile="f.rc"
+spectrumFile="None"
+# spectrumFile="f.rc"
 TypeBuild="LAST-TEST"
 TypeBuildTag="AOSP"
 getInfo ">> Building kernel . . . . <<"
 
-CompileKernel
+# CompileKernel
 
 PullPTags
 
