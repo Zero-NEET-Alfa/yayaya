74bb5dd9abf3c9ea6b35b5cd0b055a60ff5d1acb
Zero-NEET-Alfa <danidaboy54@gmail.com>
2021-03-04 00:48:30 +0700

    add do base r
    Signed-off-by: Zero-NEET-Alfa <danidaboy54@gmail.com>

---
+++ do-base-r.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+branch="base-R"
+BuilderKernel="00000"
+
+if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] ;then
+    exit;
+fi
+. main.sh 'initial' 'full'
+
+Author="ZyCromerZ"
+FolderUp="keqing-drive"
+spectrumFile="None"
+TypeBuild="Stable"
+TypeBuildTag="R-WIFI"
+getInfo ">> Building kernel . . . . <<"
+
+
+KernelFor="R"
+CompileKernel
+cd $kernelDir
+git reset --hard HEAD~1
+TypeBuildTag="P-WIFI"
+CompileKernel
+# CompileKernel "65"
+# CompileKernel "68"
+
+# PullPTags
+
+# ExFolder="STOCK-Q"
+# CompileKernel
+# CompileKernel "65"
+# CompileKernel "68"
+
+# FixPieWifi
+
+# ExFolder="STOCK-P"
+# CompileKernel
+# CompileKernel "65"
+# CompileKernel "68"
+
+
+tg_send_info "All $GetKernelName $BuilderKernel already uploaded to Gdrive :D"
