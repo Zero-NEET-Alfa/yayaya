9b3caacc8a4401a151257da11d0a27f3b0435350
ZyCromerZ <neetroid97@gmail.com>
2021-08-27 19:35:25 +0700

    push new gugel clang
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-now.sh
+++ do-now.sh
@@ -8,11 +8,19 @@ addBranch()
     git fetch origin $1
     git checkout FETCH_HEAD && git checkout -b $1
 }
-git clone https://${GIT_SECRET}@github.com/ZyCromerZ/vayu_kernel -b 20210812/main-prepare $KERNEL
+# git clone https://${GIT_SECRET}@github.com/ZyCromerZ/vayu_kernel -b 20210812/main-prepare $KERNEL
+mkdir $KERNEL
+wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r428724.tar.gz -O "clang.tar.gz"
+tar -xf clang.tar.gz -C $KERNEL
 cd $KERNEL
-git remote add myrepo https://${GIT_SECRET}@github.com/ZyCromerZ/vayu
-
-git push -f myrepo --all
+# git remote add myrepo https://${GIT_SECRET}@github.com/ZyCromerZ/vayu
+###
+git init
+git checkout -b 13.0.1-r428724
+git add . && git commit -s -m "13.0.1-master-clang-r428724"
+git remote add myrepo https://${GIT_SECRET}@github.com/ZyCromerZ/google-clang
+# git push -f myrepo --all
+git push -f myrepo 13.0.1-r428724
 cd ..
 rm -rf $KERNEL
 
