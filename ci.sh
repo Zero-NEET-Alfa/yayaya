3d9c2826cb472fa7d41ae6e77927ff7a15d7d04f
ZyCromerZ <neetroid97@gmail.com>
2021-11-23 12:33:26 +0700

    add clang builder
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- ci.sh
+++ ci.sh
@@ -26,9 +26,12 @@ function do_binutils() {
 }
 
 function do_deps() {
-    # We only run this when running on GitHub Actions
-    [[ -z ${GITHUB_ACTIONS:-} ]] && return 0
-    sudo apt-get install -y --no-install-recommends \
+        export TZ=Asia/Jakarta
+        ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+        apt-get -y update && apt-get -y upgrade && apt-get -y install git libxml2 python3-pip default-jre tzdata
+        apt-get -y install gcc llvm lld g++-multilib clang python3 python3-pip default-jre
+        apt-get -y update && apt-get -y upgrade && apt-get -y install git automake lzop bison gperf build-essential zip curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng bc libstdc++6 libncurses5 wget python3 python3-pip python gcc clang libssl-dev rsync flex git-lfs libz3-dev libz3-4 axel tar && python3 -m pip  install networkx
+        apt-get install -y --no-install-recommends \
         bc \
         bison \
         ca-certificates \
---
+++ clang-builder.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+export GIT_SSL_NO_VERIFY=1
+git config --global http.sslverify false
+
+# Set a directory
+DIR="$(pwd ...)"
+
+if [ "${1}" == "13" ];then
+    UseBranch="release/13.x"
+    CloneTo="13"
+elif [ "${1}" == "14" ];then
+    UseBranch="main"
+    CloneTo="14"
+else
+    msg "huh ???"
+    exit
+fi
+
+./build-llvm.py \
+	--clang-vendor "ZyC" \
+	--targets "ARM;AArch64;X86" \
+	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 LLVM_USE_LINKER=lld LLVM_ENABLE_LLD=ON" \
+	--pgo kernel-defconfig \
+	--lto full \
+	--shallow-clone \
+    --branch "$UseBranch"
+
+
+# Build binutils
+msg "Building binutils..."
+BIN_START=$(date +"%s")
+./build-binutils.py --targets arm aarch64 x86_64
+
+# Remove unused products
+rm -fr install/include
+rm -f install/lib/*.a install/lib/*.la
+
+# Strip remaining products
+for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
+	strip -s "${f: : -1}"
+done
+
+# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
+	# Remove last character from file output (':')
+	bin="${bin: : -1}"
+
+	echo "$bin"
+	patchelf --set-rpath "$DIR/install/lib" "$bin"
+done
+
+# Release Info
+pushd llvm-project || exit
+llvm_commit="$(git rev-parse HEAD)"
+short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
+popd || exit
+
+llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
+binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
+clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+
+git clone https://${GIT_SECRET}@github.com/ZyCromerZ/Clang -b $CloneTo $(pwd)/FromGithub
+pushd $(pwd)/FromGithub || exit
+rm -fr ./*
+cp -r ../install/* .
+echo "# Quick Info
+Clang Version : $clang_version
+Build Date : $(date +"%Y-%m-%d")" > README.md
+git add .
+git commit -asm "Update to $llvm_commit_url
+
+Clang VERSION: $clang_version
+LLVM COMMIT URL: $llvm_commit_url"
+git push -f
+popd || exit
+
+curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="-1001150624898" \
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=html" \
+    -d text="New Toolchain Already Builded boy%0ADate : <code>$(date +"%Y-%m-%d")</code>%0A<code> --- Detail Info About it --- </code>%0AClang version : <code>${clang_version}</code>%0AClang Link : <code>https://github.com/ZyCromerZ/Clang/tree/${CloneTo}</code>%0A%0A-- uWu --"
