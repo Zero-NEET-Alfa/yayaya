1a4a13071e08c599e185c2d225d7c920f5a14666
ZyCromerZ <neetroid97@gmail.com>
2021-12-10 03:55:53 +0700

    up for 'vayu-r-oss-test'
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- pushg/vayu-b.sh
+++ pushg/vayu-b.sh
@@ -4,6 +4,6 @@ GetRepo="${1}"
 shift
 GetBranch="${@}"
 [[ -z "$GetRepo" ]] && GetRepo="dob"
-[[ -z "$GetBranch" ]] && GetBranch="vayu-r-oss-b-a vayu-r-oss-b-b vayu-r-oss-b-c vayu-r-oss-b-d"
+[[ -z "$GetBranch" ]] && GetBranch="vayu-r-oss-b-a vayu-r-oss-b-b vayu-r-oss-b-c vayu-r-oss-b-d vayu-r-oss-b-e"
 SetBranch="vayu-r-oss-b"
 . pushg/vayu.sh $GetRepo $GetBranch
