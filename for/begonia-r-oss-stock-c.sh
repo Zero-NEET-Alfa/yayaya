e83ead955cfcc8834dd9e7dd0ba96f4a93c40fa2
ZyCromerZ <neetroid97@gmail.com>
2021-12-19 10:49:12 +0700

    begonia: prepare
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/begonia-r-oss-stock-c.sh
+++ for/begonia-r-oss-stock-c.sh
@@ -1,21 +1,14 @@
 #! /bin/bash
-KernelBranch="r-oss-base-release-uv"
+KernelBranch="r-oss-up"
 
 IncludeFiles "${MainPath}/device/begonia-r-oss.sh"
 CustomUploader="Y"
 # UseSpectrum="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
 # spectrumFile="None"
-FolderUp="begonia"
-TypeBuildTag="[Stable][806Mhz]"
+FolderUp="shared-file"
+TypeBuildTag="[Stable]"
 
 CloneKernel "--depth=1"
-# CloneCompiledGccTwelve
 CloneCompiledGccEleven
-CloneDTCClang
-CompileClangKernel && CleanOut
-# CloneProtonClang
-# CompileClangKernel && CleanOut
-# CompileGccKernel
-
- 
+CompileGccKernel
